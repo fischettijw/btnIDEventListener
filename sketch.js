b60aa023b1ea01539cc6fb1e2384549b9388f2d3
Joseph Fischetti <fischettijw@gmail.com>
2020-11-01 14:34:52 -0500

    working well

--- sketch.js
+++ sketch.js
@@ -1,27 +1,28 @@
-let btnArray, btnLabels;
+let btnArray, btnLabels, btnFontSize, btnFontWidth;
 
 function setup() {
     createCanvas(400, 400);
     background('yellow');
     btnArray = [];
-    btnLabels = ['First', 'Second', 'Third', 'Fourth'];
+    btnLabels = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
     btnFontSize = 18;
-    createButtons(btnArray, btnLabels, btnFontSize);
+    btnFontWidth = 150;
+    createButtons(btnArray, btnLabels, btnFontSize, btnFontWidth);
 }
 
-function createButtons() {
-    let fnt = 18;
-    for (i = 0; i < btnLabels.length; i++) {
-        let y = i * 40;
+function createButtons(btns, btnText, fntSz, fntWidth) {
+    let spacing = 5;
+    if (fntWidth == null) { fntWidth = 10 * fntSz } // fntWidth OPTIONAL
+    for (i = 0; i < btnText.length; i++) {
+        let y = i * ((2 * fntSz) + spacing); //i * 40;
+        btns[i] = createButton(btnText[i]);
+        btns[i].position(width + spacing, y + spacing);
+        btns[i].style(`font-size:${fntSz}pt`);
+        btns[i].style(`width: ${fntWidth}px; height: ${2*fntSz}px`);
+        btns[i].id(i);
         let btnID = i; // IMPORTANT TO BE HERE WITH 'LET'
-        btnArray[i] = createButton(btnLabels[i]);
-        btnArray[i].position(width, y);
-        btnArray[i].style(`font-size:${fnt}pt`);
-        btnArray[i].style(`width: ${10*fnt}px; height: ${2*fnt}px`);
-        btnArray[i].id(i);
         document.getElementById(i).onclick = function() { btnClicked(btnID); }
     }
-
 }
 
 function btnClicked(arg) {
