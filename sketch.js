206b812de0bb7ac762d34a9d03f1f84e8a40852a
Joseph Fischetti <fischettijw@gmail.com>
2020-11-01 09:52:32 -0500

    working

--- sketch.js
+++ sketch.js
@@ -13,12 +13,13 @@ function createButtons() {
     let fnt = 18;
     for (i = 0; i < btnLabels.length; i++) {
         let y = i * 40;
+        let btnID = i; // IMPORTANT TO BE HERE WITH 'LET'
         btnArray[i] = createButton(btnLabels[i]);
         btnArray[i].position(width, y);
         btnArray[i].style(`font-size:${fnt}pt`);
         btnArray[i].style(`width: ${10*fnt}px; height: ${2*fnt}px`);
         btnArray[i].id(i);
-        document.getElementById(i).onclick = function() { btnClicked(random(i, i + 1)); }
+        document.getElementById(i).onclick = function() { btnClicked(btnID); }
     }
 
 }
